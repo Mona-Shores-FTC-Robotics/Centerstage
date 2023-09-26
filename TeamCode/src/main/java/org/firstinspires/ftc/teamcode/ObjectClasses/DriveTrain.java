88b44d12380bd7dbe07ec667368f0aceed0e10ce
Dustin Shunta <dustinhans@gmail.com>
2023-09-25 21:55:58 -0400

    change to power control for now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjectClasses/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjectClasses/DriveTrain.java
@@ -85,7 +85,8 @@ public class DriveTrain {
             driveMotor[i] = Robot.getInstance().getHardwareMap().get(DcMotorEx.class, driveMotorNames[i]);
             driveMotor[i].setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
             driveMotor[i].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            driveMotor[i].setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+            //driveMotor[i].setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+            driveMotor[i].setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
             driveMotor[i].setDirection(driveMotorDirections[i]);
         }
     }
@@ -136,7 +137,7 @@ public class DriveTrain {
             }
 
             //call the drive function with the drive/turn/strafe values set based on the driver controls
-            mecanumDriveSpeedControl();
+            mecanumDrivePowerControl();
 
         } else if (!getManualDriveControlFlag()) {
             //call the drive function with the drive/turn/strafe values that are already set by vision (or some other system)
@@ -146,7 +147,7 @@ public class DriveTrain {
             drive = 0;
             strafe = 0;
             turn = 0;
-            mecanumDriveSpeedControl();
+            mecanumDrivePowerControl();
         }
     }
 
