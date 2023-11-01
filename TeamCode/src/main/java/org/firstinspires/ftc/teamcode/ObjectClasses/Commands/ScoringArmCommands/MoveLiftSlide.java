fc8b6215aa6e46cc33a97da5a92cc467014e3680
Dustin Shunta <dustinhans@gmail.com>
2023-11-01 16:42:49 -0400

    prepared comments to help teach lift code command and tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjectClasses/Commands/ScoringArmCommands/MoveLiftSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjectClasses/Commands/ScoringArmCommands/MoveLiftSlide.java
@@ -7,6 +7,7 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.arcrobotics.ftclib.command.CommandBase;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.ObjectClasses.Robot;
@@ -15,56 +16,126 @@ import org.firstinspires.ftc.teamcode.ObjectClasses.RobotSubsystems.ArmSubsystem
 
 public class MoveLiftSlide extends CommandBase {
 
-    // The subsystem the command runs on
+    private double TIMEOUT_TIME_SECONDS = 1.5;
+
+    //Declare the local variable to hold the liftsubsystem
     private final LiftSlideSubsystem liftSlideSubsystem;
 
-    //declare target state & position
+    //declare the targetState, targetTicks, currentTicks
     private LiftSlideSubsystem.LiftStates targetState;
     private int targetTicks;
+    private int currentTicks;
 
-    Telemetry telemetry;
+    //declare a timeoutTimer (type ElapsedTime) to timeout the command if it doesn't finish
+    private ElapsedTime timeoutTimer;
+
+    //declare a timeout boolean
+    boolean timeout;
 
     public MoveLiftSlide(LiftSlideSubsystem subsystem, LiftSlideSubsystem.LiftStates inputState) {
         liftSlideSubsystem = subsystem;
         targetState = inputState;
         targetTicks = targetState.ticks;
-
+        timeoutTimer = new ElapsedTime();
         //add the subsystem to the requirements
         addRequirements(liftSlideSubsystem);
     }
 
     @Override
     public void initialize() {
+        //reset the timer
+        timeoutTimer.reset();
+        //set the timeout to false since we have not timed out yet
+        timeout=false;
+
+        //get the currentTicks from the subsystem
+        currentTicks = liftSlideSubsystem.getCurrentTicks();
+
+        //Check if targetTicks is greater than MAX_TARGET_TICKS and if it is set the target to the max
+        //This makes sure that if we accidentally put a very large number as our target ticks we don't break the robot
+        if (targetTicks> liftSlideSubsystem.MAX_TARGET_TICKS)
+        {
+            targetTicks=liftSlideSubsystem.MAX_TARGET_TICKS;
+        }
+
+        //Check if targetTicks is lower than MIN_TARGET_TICKS and if it is set the target to the min
+        //This makes sure that if we accidentally put a very low negative number as our target ticks we don't break the robot
+        if (targetTicks < liftSlideSubsystem.MIN_TARGET_TICKS)
+        {
+            targetTicks=liftSlideSubsystem.MIN_TARGET_TICKS;
+        }
+
+        //if the target ticks are higher than the current ticks, then use EXTENSION_POWER
+        if (targetTicks > currentTicks) {
+            liftSlideSubsystem.liftSlide.setPower(LiftSlideSubsystem.liftSlideParameters.EXTENSION_LIFT_POWER);
+        }
+
+        //if the target ticks are lower than the current ticks, then use RETRACTION_POWER
+        if (targetTicks < currentTicks) {
+            liftSlideSubsystem.liftSlide.setPower(LiftSlideSubsystem.liftSlideParameters.RETRACTION_LIFT_POWER);
+        }
+
+        //Set the target position using the targetTicks
         liftSlideSubsystem.liftSlide.setTargetPosition(targetTicks);
+
+        //set the lift motor to RUN TO POSITION - this might not be necessary
         liftSlideSubsystem.liftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        //create a new telemetry packet for this command
-        telemetry = Robot.getInstance().getActiveOpMode().telemetry;
+        //Make a new TelemetryPacket
+        TelemetryPacket p = new TelemetryPacket();
 
+        //Put the target state in the packet
+        p.put("Target LiftSlide State", targetState);
+        //put the target ticks in the packet
+        p.put("Target Ticks", targetTicks);
+
+        //send the packet to the dashboard
+        FtcDashboard.getInstance().sendTelemetryPacket(p);
     }
 
     public void execute() {
-        liftSlideSubsystem.currentTicks = liftSlideSubsystem.liftSlide.getCurrentPosition();
-
-        TelemetryPacket p = new TelemetryPacket();
-        p.put("Target LiftSlide State", targetState);
-        p.put("Target Ticks", targetTicks);
-        FtcDashboard.getInstance().sendTelemetryPacket(p);
+        //The execute of this command is empty because we don't need to do anything while the command is active
+        //the RUN_TO_POSITION we set in the intialize means that the lift motor is going to automatically try to reach the target
+        //there is telemetry in the periodic() method of the lift subsystem that will give us updates in the dashboard about the current ticks and current state
     }
 
     @Override
     public boolean isFinished() {
-        boolean finished = Math.abs(liftSlideSubsystem.currentTicks - targetTicks) <  LiftSlideSubsystem.liftSlideParameters.LIFT_HEIGHT_TICK_THRESHOLD;
+
+        //get the currentTicks from the subsystem because we need it to figure out if we have reached our target
+        currentTicks = liftSlideSubsystem.getCurrentTicks();
+
+        // Declare a boolean variable (e.g., finished)
+        // Compare the currentTicks to the targetTicks to a threshold (LIFT_HEIGHT_TICK_THRESHOLD) and save as the boolean
+        // For example, say our target is 2000 ticks and we are at 1997 - we would want that to count as being close enough
+        boolean finished = Math.abs(currentTicks - targetTicks) <  LiftSlideSubsystem.liftSlideParameters.LIFT_HEIGHT_TICK_THRESHOLD;
+
+        //write an if statement to change the currentState to the targetState and return true if the finished boolean is true
         if (finished){
-            liftSlideSubsystem.currentState = targetState;
+            //if the command is finished, then change the current state to the target state and return true (meaning the command will stop running)
+            liftSlideSubsystem.setCurrentState(targetState);
             return true;
-        } else return false;
+        }
+
+        //compare the elapsed time to a timeout threshold and if the elapsed time is greater than the threshold return true
+        timeout = timeoutTimer.seconds() > TIMEOUT_TIME_SECONDS;
+        if (timeout){
+            return true;
+        }
+
+        //if the command isn't finished and the command isn't timed out then return false because the command should still run
+        return false;
     }
 
     @Override
     public void end(boolean interrupted) {
-        if (interrupted){
-            //what should we do if interrupted?
+        //write an if statement that tells the user the command didn't finish normally but instead timed out
+        if (timeout){
+            TelemetryPacket p = new TelemetryPacket();
+            //Put the target state in the packet
+            p.addLine("Slide Move Timeout");
+            p.put("Timeout Timer", timeoutTimer.seconds());
+            FtcDashboard.getInstance().sendTelemetryPacket(p);
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjectClasses/RobotSubsystems/ArmSubsystems/LiftSlideSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjectClasses/RobotSubsystems/ArmSubsystems/LiftSlideSubsystem.java
@@ -1,37 +1,53 @@
 package org.firstinspires.ftc.teamcode.ObjectClasses.RobotSubsystems.ArmSubsystems;
 
-import androidx.annotation.NonNull;
-
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.Action;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+import org.firstinspires.ftc.teamcode.ObjectClasses.Robot;
 
 @Config
 public class LiftSlideSubsystem extends SubsystemBase {
 
     public static class LiftSlideParameters {
         public int LIFT_HEIGHT_TICK_THRESHOLD = 15;
-        public double STARTING_LIFT_POWER = .6;
-        public double VEL_P=5, VEL_I=0, VEL_D=0, VEL_F=30;
+        public double EXTENSION_LIFT_POWER = .6;
+        public double RETRACTION_LIFT_POWER = .3;
+        public double VEL_P=5, VEL_I=0, VEL_D=0, VEL_F=28;
         public double POS_P=5;
     }
+    public static class LiftSlideHeights{
+        public int HOME_HEIGHT_TICKS=5;
+        public int LOW_HEIGHT_TICKS=800;
+        public int MID_HEIGHT_TICKS=1700;
+        public int HIGH_HEIGHT_TICKS=2400;
+    }
+
+    public int MAX_TARGET_TICKS = 2500;
+    public int MIN_TARGET_TICKS = 0;
 
     public static LiftSlideParameters liftSlideParameters = new LiftSlideParameters();
+    public static LiftSlideHeights liftSlideHeights = new LiftSlideHeights();
 
     public enum LiftStates {
-        HIGH (2200),
-        MID(1700),
-        LOW (1200),
-        HOME (125);
-
+        HIGH, MID, LOW, HOME;
         public int ticks;
+
+        static {
+            HIGH.ticks = liftSlideHeights.HIGH_HEIGHT_TICKS;
+            MID.ticks = liftSlideHeights.MID_HEIGHT_TICKS;
+            LOW.ticks = liftSlideHeights.LOW_HEIGHT_TICKS;
+            HOME.ticks = liftSlideHeights.HOME_HEIGHT_TICKS;
+        }
+
+        public void setLiftHeightTicks(int t){
+            this.ticks = t;
+        }
 
         //From ChatGPT
         //C=π×d
@@ -39,22 +55,24 @@ public class LiftSlideSubsystem extends SubsystemBase {
         //C=3.14159inch circumference
         //Gobilda 5203 motor outputs 8192 ticks for one full revolution of motor shaft
         // So ~2608 ticks per inch.
-        //this did not work at all.
-
-        LiftStates(int t) {
-            this.ticks = t;
-        }
+        //this did not work at all - why not?
     }
 
     public DcMotorEx liftSlide;
-    public PIDFCoefficients pidfCoefficients;
 
-    public LiftStates currentState;
-    public int currentTicks;
-    public double currentDistanceInches;
-    FtcDashboard dash;
-    public double power;
-    TelemetryPacket telemetryPacket;
+    private LiftStates currentState;
+    public void setCurrentState(LiftStates state) {
+        this.currentState = state;
+    }
+
+    private int currentTicks;
+    public int getCurrentTicks() {
+        return currentTicks;
+    }
+
+    private double power;
+    private FtcDashboard dash;
+    private TelemetryPacket telemetryPacket;
 
     /** Constructor **/
     public LiftSlideSubsystem(final HardwareMap hMap, final String name) {
@@ -69,11 +87,11 @@ public class LiftSlideSubsystem extends SubsystemBase {
         liftSlide.setVelocityPIDFCoefficients(liftSlideParameters.VEL_P,liftSlideParameters.VEL_I,liftSlideParameters.VEL_D,liftSlideParameters.VEL_F);
         liftSlide.setPositionPIDFCoefficients(liftSlideParameters.POS_P);
         liftSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        power = liftSlideParameters.STARTING_LIFT_POWER;
+        power = liftSlideParameters.EXTENSION_LIFT_POWER;
         liftSlide.setPower(power);
+
         currentState = LiftStates.HOME;
         currentTicks = LiftStates.HOME.ticks;
-        currentDistanceInches = LiftStates.HOME.ticks;
         liftSlide.setTargetPosition(currentTicks);
         liftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         dash = FtcDashboard.getInstance();
@@ -82,7 +100,13 @@ public class LiftSlideSubsystem extends SubsystemBase {
     public void periodic(){
         liftSlide.setVelocityPIDFCoefficients(liftSlideParameters.VEL_P,liftSlideParameters.VEL_I,liftSlideParameters.VEL_D,liftSlideParameters.VEL_F);
         liftSlide.setPositionPIDFCoefficients(liftSlideParameters.POS_P);
-        liftSlide.setPower(liftSlideParameters.STARTING_LIFT_POWER);
+
+        LiftStates.HOME.setLiftHeightTicks(liftSlideHeights.HOME_HEIGHT_TICKS);
+        LiftStates.LOW.setLiftHeightTicks(liftSlideHeights.LOW_HEIGHT_TICKS);
+        LiftStates.MID.setLiftHeightTicks(liftSlideHeights.MID_HEIGHT_TICKS);
+        LiftStates.HIGH.setLiftHeightTicks(liftSlideHeights.HIGH_HEIGHT_TICKS);
+
+        currentTicks = liftSlide.getCurrentPosition();
 
         telemetryPacket = new TelemetryPacket();
         telemetryPacket.put("Current LiftSlide State", currentState);
