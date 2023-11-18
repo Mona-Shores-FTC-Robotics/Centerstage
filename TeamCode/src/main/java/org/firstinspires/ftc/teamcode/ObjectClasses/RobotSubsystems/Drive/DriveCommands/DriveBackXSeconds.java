0ad77ebadcb45a9657355f27a454cf0d297f5b9f
hopeshunta <hope.shunta@gmail.com>
2023-11-17 19:54:20 -0500

    binding

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjectClasses/RobotSubsystems/Drive/DriveCommands/DriveBackXSeconds.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.ObjectClasses.RobotSubsystems.Drive.DriveCommands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.ObjectClasses.RobotSubsystems.Drive.DriveSubsystem;
+import org.firstinspires.ftc.teamcode.ObjectClasses.RobotSubsystems.Drive.MecanumDriveMona;
+
+/**
+ * A command to drive the robot with joystick input *
+ */
+public class DriveBackXSeconds extends CommandBase {
+
+    private final DriveSubsystem driveSubsystem;
+    private ElapsedTime timer;
+    private MecanumDriveMona mecanumDrive;
+    private double time;
+    /**
+     * Creates a new DefaultDrive.
+     */
+    public DriveBackXSeconds(DriveSubsystem subsystem, double timeToDrive) {
+        driveSubsystem = subsystem;
+        time = timeToDrive;
+        addRequirements(driveSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        timer = new ElapsedTime();
+        timer.reset();
+    }
+
+    @Override
+    public void execute() {
+        //this sets the drive/strafe/turn values based on the values supplied, while also doing automatic apriltag driving to the backdrop
+        if (timer.seconds() < time) {
+            driveSubsystem.mecanumDrive.mecanumDriveSpeedControl(.2, 0, 0);
+        } else driveSubsystem.mecanumDrive.mecanumDriveSpeedControl(0, 0, 0);
+    }
+}
