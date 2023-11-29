32c7282f060162e04d65e249dea84bf1ed7697ef
hopeshunta <hope.shunta@gmail.com>
2023-11-28 19:46:58 -0600

    roadrunner parameters from Fruitport

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjectClasses/RobotSubsystems/Drive/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjectClasses/RobotSubsystems/Drive/DriveSubsystem.java
@@ -53,9 +53,10 @@ public class DriveSubsystem extends SubsystemBase {
     }
 
     public static class ParamsRRMona {
-        public double inPerTick = 0.04122; // 0.0317919075144509
-        public double lateralInPerTick =0.04329; // 60\1845.5 .025
+        public double inPerTick = 0.0313;  // was 0.04122; lower numbers move robot more
+        public double lateralInPerTick =0.0283;        // was 0.04329; lower numbers move robot more
         public double trackWidthTicks =631.8289216104534;  //631.8289216104534
+
 
         //new values
         public double kS = 0.9574546275336608;  //0.9574546275336608
