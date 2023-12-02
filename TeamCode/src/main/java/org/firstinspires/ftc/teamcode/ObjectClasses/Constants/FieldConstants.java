d1e2a71d73b02485a6b892685161376b47690d15
hopeshunta <hope.shunta@gmail.com>
2023-12-02 09:35:10 -0600

    blue audience adjustments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjectClasses/Constants/FieldConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ObjectClasses/Constants/FieldConstants.java
@@ -63,12 +63,12 @@ public class FieldConstants {
 
     //  public static Pose2d BLUE_BACKDROP_RIGHT = new Pose2d(2*TILE, 29.264, FACE_TOWARD_BACKSTAGE);
 //  public static Pose2d RED_BACKDROP_LEFT = new Pose2d(2*TILE, -29.264, FACE_TOWARD_BACKSTAGE);
-    public static Pose2d BLUE_BACKDROP_RIGHT = new Pose2d(2 * TILE, TILE + HALF_TILE - QUARTER_TILE - 1.5, FACE_TOWARD_BACKSTAGE);
+    public static Pose2d BLUE_BACKDROP_RIGHT = new Pose2d(2 * TILE, TILE + HALF_TILE - QUARTER_TILE+ 1, FACE_TOWARD_BACKSTAGE);
     public static Pose2d RED_BACKDROP_LEFT = new Pose2d(2 * TILE, -TILE - QUARTER_TILE + 1.5, FACE_TOWARD_BACKSTAGE);
 
     //  public static Pose2d BLUE_BACKDROP_CENTER = new Pose2d(2*TILE, 35.264, FACE_TOWARD_BACKSTAGE);
 //  public static Pose2d RED_BACKDROP_CENTER = new Pose2d(2*TILE, -35.264, FACE_TOWARD_BACKSTAGE);
-    public static Pose2d BLUE_BACKDROP_CENTER = new Pose2d(2 * TILE, TILE + HALF_TILE, FACE_TOWARD_BACKSTAGE);
+    public static Pose2d BLUE_BACKDROP_CENTER = new Pose2d(2 * TILE, TILE + HALF_TILE+1.5, FACE_TOWARD_BACKSTAGE);
     public static Pose2d RED_BACKDROP_CENTER = new Pose2d(2 * TILE, -TILE - HALF_TILE, FACE_TOWARD_BACKSTAGE);
 
     //  public static Pose2d BLUE_BACKDROP_LEFT = new Pose2d(2*TILE, 41.264, FACE_TOWARD_BACKSTAGE);
@@ -91,7 +91,7 @@ public class FieldConstants {
     public static Pose2d RED_BACKSTAGE_SPIKE_C_PAST = new Pose2d(HALF_TILE + 3, -TILE - QUARTER_TILE, FACE_TOWARD_BLUE);
     public static Pose2d RED_BACKSTAGE_SPIKE_C_DROP = new Pose2d(HALF_TILE + 3, -TILE - QUARTER_TILE - 4, FACE_TOWARD_BLUE);
 
-    public static Pose2d RED_AUDIENCE_SPIKE_C = new Pose2d(-TILE - HALF_TILE, -TILE - HALF_TILE + 2, FACE_TOWARD_BLUE);
+    public static Pose2d RED_AUDIENCE_SPIKE_C = new Pose2d(-TILE - HALF_TILE, -TILE - HALF_TILE + 1, FACE_TOWARD_BLUE);
 
     public static Pose2d RED_AUDIENCE_SPIKE_C_PAST = new Pose2d(-TILE - HALF_TILE + 3, -TILE - QUARTER_TILE, FACE_TOWARD_BLUE);
     public static Pose2d RED_AUDIENCE_SPIKE_C_DROP = new Pose2d(-TILE - HALF_TILE + 3, -TILE - QUARTER_TILE - 4, FACE_TOWARD_BLUE);
@@ -101,7 +101,7 @@ public class FieldConstants {
     public static Pose2d RED_BACKSTAGE_SPIKE_R_PAST = new Pose2d(TILE - QUARTER_TILE + 1, -TILE - HALF_TILE + 5, Math.toRadians(45));
     public static Pose2d RED_BACKSTAGE_SPIKE_R_DROP = new Pose2d(TILE - QUARTER_TILE - .5, -TILE - HALF_TILE + 3, Math.toRadians(45));
 
-    public static Pose2d RED_AUDIENCE_SPIKE_R = new Pose2d(-TILE - QUARTER_TILE, -TILE - HALF_TILE, FACE_45_DEGREES);
+    public static Pose2d RED_AUDIENCE_SPIKE_R = new Pose2d(-TILE - QUARTER_TILE-1, -TILE - HALF_TILE, FACE_45_DEGREES);
     public static Pose2d RED_AUDIENCE_SPIKE_R_PAST = new Pose2d(-TILE - QUARTER_TILE + 2, -TILE - HALF_TILE + 2.5, Math.toRadians(65));
     public static Pose2d RED_AUDIENCE_SPIKE_R_DROP = new Pose2d(-TILE - QUARTER_TILE + 1.3, -TILE - HALF_TILE + 1, Math.toRadians(65));
 
